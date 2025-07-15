bebcab9542c3818055591c08482a1812371c8633
Ayushd785 <ayushd785@gmail.com>
2025-07-15 14:21:57 +0530

    added getProfile routes

--- controller/authController.js
+++ controller/authController.js
@@ -80,4 +80,23 @@ const login = async (req, res) => {
   }
 };
 
+
+const getProfile = async (req,res)=>{
+  try{const userId = req.userId;
+    const user = await User.findById(userId).select("-password");
+    if(!user){
+      return res.status(404).json({
+        msg:"User not found"
+      })
+    }
+    return res.status(200).json({
+      msg:"User profile fetched successfully",
+      user
+    })
+  }catch(err){
+    return res.status(500).json({
+      msg:"Server error",
+      error: err.message
+    })
+}
 module.exports = { signup, login };
